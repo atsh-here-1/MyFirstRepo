9b545ce243d05b18f1482528817c3f1c9dc12015
Anant Singh <iop.user.1234@gmail.com>
2025-07-31 16:16:51 +0530

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,15 @@
+// firebase-init.js
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJgwoXtuQBjxvjZNBaFaobgmVjVJNKgqM",
+  authDomain: "ascandane.firebaseapp.com",
+  projectId: "ascandane",
+  storageBucket: "ascandane.appspot.com",
+  messagingSenderId: "389414086414",
+  appId: "1:389414086414:web:e83f36f864afb41861595f",
+  measurementId: "G-NMXHWJ4GRJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
